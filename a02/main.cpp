2001ebdd0a6f8bb79f76eb8044ab880c45e74fdd
Yuki-Hideo <buchojunsa@gmail.com>
2026-05-31 14:55:50 +0900

    a02

---
+++ a02/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n, x;
+  cin >> n >> x;
+  vector<int> a(n);
+
+  for(int i = 0; i < n; i++) {
+    cin >> a[i];
+    if(a[i] == x) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+
+  return 0;
+}
